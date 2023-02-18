0917463fc57147180c7f024762c885e35c08acbf
aminLuis <aminluisamin@gmail.com>
2023-02-18 10:49:51 -0500

    Commit inicial, inicio API drogueria

---
+++ src/main/java/com/apidrogueria/apidrogueria/ApiDrogueriaApplication.java
@@ -0,0 +1,13 @@
+package com.apidrogueria.apidrogueria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDrogueriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDrogueriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apidrogueria/apidrogueria/ApiDrogueriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apidrogueria.apidrogueria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDrogueriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
